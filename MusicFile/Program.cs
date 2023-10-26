c4c4e4c2b4e615c8bc802256b8da76c4b877ec85
Антисептик <@DESKTOP-KTN6EI8>
2023-10-26 12:45:36 +0300

    Добавьте файлы проекта.

---
+++ MusicFile/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+HttpClient client = new HttpClient();
+HttpResponseMessage response = await client.GetAsync("https://mp3party.net/music/10817608.mp3");
+byte[] data = response.Content.ReadAsByteArrayAsync().Result;
+File.WriteAllBytes("C:\\Users\\Антисептик\\Desktop\\manera.mp3", data);
+
+Process.Start(new ProcessStartInfo { FileName = "C:\\Users\\Антисептик\\Desktop\\manera.mp3", UseShellExecute = true });
